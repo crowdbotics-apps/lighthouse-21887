a2e24ffed6d00a8203083fb51c6db462834ab02f
Crowdbotics <team@crowdbotics.com>
2020-10-23 22:35:57 +0000

    Update components

--- src/features/BlankScreen7158787/screens/blank.js
+++ src/features/BlankScreen7158787/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,43 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
-    </View>
-  );
+    <ImageBackground
+      source={{
+        uri:
+          "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/e1cd984697b5fc0533cbedac8f77dde4.jpg"
+      }}
+    >
+      <Text>
+        The take five method is a grounding exercise that is commonly used for
+        anxiety disorders. 1. look around and find five things you can see 2.
+        close your eyes and find five things you can feel don&#39;t forget to
+        breath 3. close your eyes again and find 3 things you can hear 4.close
+        your eyes again and find 2 things you can smell 5. what is one thing you
+        can taste
+      </Text>
+    </ImageBackground>
+  )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  container: { flex: 1, marginHorizontal: 16 },
+  ImageBackground_1: {},
+  Text_2: {}
+})
