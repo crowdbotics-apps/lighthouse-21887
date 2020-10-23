5dc0556205b8355541eb6ed2a134c1b99f81b07d
Crowdbotics <team@crowdbotics.com>
2020-10-23 22:31:08 +0000

    Update components

--- src/features/BlankScreen10158790/screens/blank.js
+++ src/features/BlankScreen10158790/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,54 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
-    </View>
-  );
+    <ImageBackground
+      source={{
+        uri:
+          "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/e72ce29bc3f6b207cdbdc74b8690ffee.jpg"
+      }}
+      style={styles.ImageBackground_1}
+    >
+      <Text>goal setting and time management</Text>
+      <Text>
+        Clarify values: Relax and contemplate; years from now, you are in your
+        favorite place, you lived a full and happy life, looking back what are
+        you most proud of accomplishing? What are you most grateful for? what is
+        your favorite memory? Relax again; you are back at your current age, you
+        are at your favorite place, you were just diagnosed with an illness that
+        showed no symptoms but would kill you in six months, what will you do
+        with the half a yeah you have left? Compare your values from the two
+        exercises Setting Goals 1 is it a goal that you are willing to devote
+        your time to? or is it a dream you wish to fall in your lap? 2 Is it
+        consistent with your values? 3 Is this goal achievable? 4 Is it a
+        positive goal? goals to move toward instead of away from, instead of
+        &#39;stop ignoring homework&#39; &#39;Try to get 3 assignments phone
+        today!&quot; 5 are your goals balanced? often times goals will focus on
+        fiances and business, try to add health and fun related goals too!{" "}
+      </Text>
+    </ImageBackground>
+  )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  container: { flex: 1, marginHorizontal: 16 },
+  ImageBackground_1: { color: "#ffffff" },
+  Text_2: {},
+  Text_3: {}
+})
